c5cb342600a62dabc4b7863bf8c6c0e2e55335f0
Nam Mai <maihoangnam.study@gmail.com>
2025-07-20 17:17:40 +0200

    Adding header, labels, buttons and fix CSS

---
+++ tolockin/src/App.js
@@ -0,0 +1,71 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      */}
+
+      <h1>ToLockIn</h1>
+
+      {/* To-do input items */}
+      <div className='todo-wrapper'>
+        
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="Task's title" />
+          </div>
+
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type="text" placeholder="Task's description" />
+          </div>
+
+          <div className='todo-input-item'>
+            <label>Priority</label>
+              <select>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+          </div>
+
+          <div className='todo-input-item'>
+            <button type='button'>Add task</button>
+          </div>
+        </div>
+
+        <div className='todo-output'>
+          <div className='btn-area'>
+            <button>Todo</button>
+            <button>Completed</button>
+          </div>
+
+          <div>
+            <h2>Task 1</h2>
+            <label>Description</label>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
